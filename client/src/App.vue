30cf7653d79e1fc2bd265e0b1349b2ea94adb1fd
reustonium <andrew.ruestow@gmail.com>
2016-11-28 15:13:57 -0500

    Add Header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,37 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-    <div class="tile is-ancestor">
-      <div class="tile is-parent">
-        <div class="tile is-child box">
-          <h1>hi</h1>
-        </div>
-      </div>
-    </div>
+    <app-header></app-header>
+    <h1>test</h1>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import AppHeader from './components/AppHeader'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    AppHeader
   }
 }
 </script>
-
-<style>
-@import "bulma"
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import '~bulma';
 </style>
---
+++ client/src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="hero is-bold app-header">
+    <div class="hero-head">
+      <nav class="nav has-shadow">
+        <div class="nav-center">
+          <a class="nav-item hero-header" href="/">
+            <span class="uptime">Uptime</span> <strong class="webapp">Webapp</strong>
+          </a>
+        </div>
+      </nav>
+    </div>
+  </section>
+</template>
+<script>
+</script>
+<style lang="scss">
+.app-header {
+  position: fixed;
+  min-width: 100%;
+  box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
+}
+
+.hero-header {
+  font-size: 1.35em;
+  .uptime {
+    margin-left: 10px;
+    color: #36AC70;
+  }
+  .webapp {
+    color: #28374B;
+  }
+}
+</style>
