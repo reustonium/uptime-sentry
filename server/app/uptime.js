29a496a046313399f742b3cf6771097ba6ac057b
reustonium <andrew.ruestow@gmail.com>
2017-02-18 17:02:44 -0500

    Add ping filtering

--- server/app/uptime.js
+++ server/app/uptime.js
@@ -1,4 +1,9 @@
+let moment = require('moment');
+
 function calculateUptime(pings) {
+  var daily = pings.filter(dailyPings);
+  var weekly = pings.filter(weeklyPings);
+  var monthly = pings.filter(monthlyPings);
   return {
     day: 100,
     week: 98,
@@ -6,6 +11,31 @@ function calculateUptime(pings) {
   }
 }
 
+// Keep the pings that happened within the last 1 day
+function dailyPings(ping) {
+  let aDayAgo = moment().subtract(1, 'days');
+  return moment(ping.pingedAt).isSameOrAfter(aDayAgo);
+}
+
+// Keep the pings that happened within the last 7 days
+var weeklyPings = function(ping) {
+  let aWeekAgo = moment().subtract(7, 'days');
+  return moment(ping.pingedAt).isSameOrAfter(aWeekAgo);
+}
+
+// Keep the pings that happened within the last 1 month
+var monthlyPings = function(ping) {
+  let aMonthAgo = moment().subtract(1, 'months');
+  return moment(ping.pingedAt).isSameOrAfter(aMonthAgo);
+}
+
+// Calculate the Uptime for the pings
+var uptime = function(pings) {}
+
 module.exports = {
-  calculateUptime
+  calculateUptime: calculateUptime,
+  dailyPings: dailyPings,
+  weeklyPings: weeklyPings,
+  monthlyPings: monthlyPings,
+  uptime: uptime
 }
---
+++ server/test/uptime.js
@@ -0,0 +1,68 @@
+process.env.NODE_ENV = 'test';
+let chai = require('chai');
+let should = chai.should();
+let expect = chai.expect;
+let Uptime = require('../app/uptime');
+let moment = require('moment');
+
+describe('Uptime', () => {
+
+  describe('calculateUptime', () => {
+    it('should return 100, 98 and 97', () => {
+      expect(1).to.equal(1);
+      expect(Uptime.calculateUptime([{}, {}])).to.eql({
+        day: 100,
+        week: 98,
+        month: 97
+      });
+    });
+  });
+
+  describe('dailyPings', () => {
+    it('should return a ping that occured in the last day', () => {
+      let todayPing = {
+        pingedAt: moment().subtract(1, 'hour')
+      };
+      expect(Uptime.dailyPings(todayPing)).to.be.ok;
+    });
+
+    it('should filter out a ping that occured more than a day ago', () => {
+      let oldPing = {
+        pingedAt: moment().subtract(2, 'days')
+      };
+      expect(Uptime.dailyPings(oldPing)).to.not.be.ok;
+    });
+  });
+
+  describe('weeklyPings', () => {
+    it('should return a ping that occured in the last week', () => {
+      let recentPing = {
+        pingedAt: moment().subtract(2, 'days')
+      };
+      expect(Uptime.weeklyPings(recentPing)).to.be.ok;
+    });
+
+    it('should filter out a ping that occured more than a week ago', () => {
+      let oldPing = {
+        pingedAt: moment().subtract(2, 'weeks')
+      };
+      expect(Uptime.weeklyPings(oldPing)).to.not.be.ok;
+    });
+  });
+
+  describe('monthlyPings', () => {
+    it('should return a ping that occured in the last month', () => {
+      let recentPing = {
+        pingedAt: moment().subtract(2, 'weeks')
+      };
+      expect(Uptime.monthlyPings(recentPing)).to.be.ok;
+    });
+
+    it('should filter out a ping that occured more than a month ago', () => {
+      let oldPing = {
+        pingedAt: moment().subtract(2, 'months')
+      };
+      expect(Uptime.monthlyPings(oldPing)).to.not.be.ok;
+    });
+  });
+});
